875f3d5c1eeaa7537803b4ba8d43c34ceb4f3eb6
Hunter Schmidt <epicschmidt@gmail.com>
2020-04-18 23:30:51 -0600

    alice requests from Bob, Bob creates reply correctly.

--- PA3/alice/Alice.py
+++ PA3/alice/Alice.py
@@ -1,17 +1,29 @@
 from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+
 from socket import *
 from optparse import OptionParser
+
+from base64 import b64decode, b64encode
+import json
+import os
 
 class message():
     message=''
     metadata=''
 
-    def __init__(self, message, metadata):
-        self.message = message
-        self.metadata = metadata
+    def __init__(self, message, metadata="TO_DECODE"):
+        if(metadata == "TO_DECODE"):
+            self.decode(message)
+        else:
+            self.message = message
+            self.metadata = metadata
 
-    # def __init__(self, encoded_message):
-    #     json_message = json.loads()
+    def decode(self, blob):
+        obj_dict = json.loads(blob)
+        self.message = obj_dict['message']
+        self.metadata = obj_dict['metadata']
 
     def encode(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
@@ -21,31 +33,56 @@ def main():
     parser = OptionParser()
 
     parser.add_option("-H","--host",dest="bob_address",type="string",default="localhost",help="Specifies hostname/ip address of Bob script. Default is localhost.")
-    parser.add_option("-p","--port",dest="port", type="int",default=2100,help="Specifies port to connect to Bob program on. Default is 2100.")
+    parser.add_option("-p","--port",dest="port", type="int",default=11111,help="Specifies port to connect to Bob program on. Default is 2100.")
     (options,args) = parser.parse_args()
 
     connect_to_bob(options.bob_address, options.port)
 
-def connect_to_bob(hostname, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((hostname,port))    
+def verify_message(message_in, key_location):
+    data_in = message_in.message
+    digest_in = message_in.metadata
+    here = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(here, key_location)
+    key = RSA.importKey(open(filename).read())
+    h = SHA.new(data_in)
+    verifier = PKCS1_v1_5.new(key)
+    return verifier.verify_message(h, digest_in)
 
+
+
+def connect_to_bob(hostname, port):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((hostname,port))    
+    request_pubkey = message("REQUEST", "PUBKEY")
+    s.sendall(request_pubkey.encode().encode())
     data = receive_blob(s)
+    message_in = message(data)
+    if(verify_message(message_in)):
+        f = open("bobpublickey.pem","x")
+        f.write(b64decode(message_in.message).decode())
+
 
 def receive_blob(the_socket):
     message = ''
     data = ''
-    the_socket.settimeout(5)   
+    #the_socket.settimeout(5000000)   
 
     try:
         while('{' not in message[:1]):
             data = the_socket.recv(1024)
-            message+= data
-        while('{' in message[:1] and '}' not in message[:-1]):
-            data = the_socket.recv(1024)
+            if(data is None):
+                continue
+            else:
+                data = data.decode()
+                message+= data
+        while('{' in message[:1] and '}' not in message[-1:]):
+            data = the_socket.recv(1024).decode()
             message+= data
     except timeout:
         print("Connection timed out!")
         return
 
     return message
+
+if __name__ == "__main__":
+    main()
--- PA3/bob/Bob.py
+++ PA3/bob/Bob.py
@@ -1,19 +1,28 @@
 from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+
 from socket import *
 from optparse import OptionParser
 import json
-
+from base64 import b64encode, b64decode
+import os
 
 class message():
     message=''
     metadata=''
 
-    def __init__(self, message, metadata):
-        self.message = message
-        self.metadata = metadata
+    def __init__(self, message, metadata="TO_DECODE"):
+        if(metadata == "TO_DECODE"):
+            self.decode(message)
+        else:
+            self.message = message
+            self.metadata = metadata
 
-    # def __init__(self, encoded_message):
-    #     json_message = json.loads()
+    def decode(self, blob):
+        obj_dict = json.loads(blob)
+        self.message = obj_dict['message']
+        self.metadata = obj_dict['metadata']
 
     def encode(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
@@ -25,39 +34,66 @@ def main():
     parser = OptionParser()
 
     #parser.add_option("-h","--host",dest="bob_address",type="string",default="localhost",help="Specifies hostname/ip address of Bob script. Default is localhost.")
-    parser.add_option("-p","--port",dest="port", type="int",default=2100,help="Specifies port to listen for Alice program on. Default is 2100.")
+    parser.add_option("-p","--port",dest="port", type="int",default=11111,help="Specifies port to listen for Alice program on. Default is 2100.")
     (options,args) = parser.parse_args()
 
-    test = message("test message", "test metadata")
-    print(test.encode()) 
+    # test = message("test message", "test metadata")
+    # encoded_test = test.encode()
+    # print(encoded_test) 
+    # test_receive = message(encoded_test)
+    start_server(options.port)
+    
 
     
 
 def start_server(port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind('',int(port))
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('',int(port)))
     server_socket.listen(1)
     print("Server listening on port: " + str(port))
-    #while True:
     connection_socket, addr = server_socket.accept()
-    init_request = connection_socket.receive_blob()
+    print("Alice connected!")
+    while True:
+        init_request = receive_blob(connection_socket)
+        if(init_request is None):
+            continue
+        else:
+            message_in = message(init_request)
+            handle_request(connection_socket, message_in)
 
+def handle_request(the_socket, request):
+    if(request.message=="REQUEST" and request.metadata=="PUBKEY"):
+        send_pubkey(the_socket)
 
-    
+def send_pubkey(the_socket):
+    bob_pubkey = file_to_b64_string('bobpublickey.pem')
+    signed_digest = sign_message(bob_pubkey,'caprivatekey.pem')
+    to_send = message(bob_pubkey,signed_digest).encode()
+    the_socket.sendall(to_send.encode())
 
-    data = receive_blob(server_socket)
+def file_to_b64_string(filename):
+    here = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(here, filename)
+    with open(filename,'r') as file:
+        data = file.read()
+    b64_data = b64encode(data.encode())
+    return b64_data
 
 def receive_blob(the_socket):
     message = ''
     data = ''
-    the_socket.settimeout(5)   
+    #the_socket.settimeout(5000000)   
 
     try:
         while('{' not in message[:1]):
             data = the_socket.recv(1024)
-            message+= data
-        while('{' in message[:1] and '}' not in message[:-1]):
-            data = the_socket.recv(1024)
+            if(data is None):
+                continue
+            else:
+                data = data.decode()
+                message+= data
+        while('{' in message[:1] and '}' not in message[-1:]):
+            data = the_socket.recv(1024).decode()
             message+= data
     except timeout:
         print("Connection timed out!")
@@ -65,7 +101,18 @@ def receive_blob(the_socket):
 
     return message
 
+def sign_message(data, key_location):
+    here = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(here, key_location)
+    key = open(filename,'r').read()
+    rsakey = RSA.importKey(key)
+    signer = PKCS1_v1_5.new(rsakey)
+    digest = SHA.new()
 
+    digest.update(b64decode(data))
+    sign = signer.sign(digest)
+
+    return b64encode(sign)
 
 
 if __name__ == "__main__":
